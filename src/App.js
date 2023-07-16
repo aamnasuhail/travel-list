cf5160c7c0af77027bc3f52585175b9f439a85a9
Aamna Suhail <aamnasuhail13213@gmail.com>
2023-07-16 23:25:25 +0530

    Clear list and Sorted Items

--- src/App.js
+++ src/App.js
@@ -20,7 +20,12 @@ function App() {
       )
     );
   }
-
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items"
+    );
+    if (confirmed) setItems([]);
+  }
   return (
     <div className="app">
       <Logo />
@@ -29,6 +34,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
